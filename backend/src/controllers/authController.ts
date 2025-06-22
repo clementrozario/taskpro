fe51459454ec467523b9648155a1aa27a7e6482c
Clement Delphin <clementcla@outlook.com>
2025-06-22 13:25:02 +0530

    validation added to auth using yup

--- backend/src/controllers/authController.ts
+++ backend/src/controllers/authController.ts
@@ -2,10 +2,25 @@ import { Request,Response } from "express";
 import bcrypt from 'bcryptjs'
 import User from '../models/User';
 import jwt from 'jsonwebtoken';
+import * as yup from 'yup'
+
+const signupSchema = yup.object({
+    email:yup.string().email("Invalid email").required("Email is required"),
+    password:yup.string().min(6,"password must be atleast 6 charectors").required("password is required"),
+    role:yup.string().oneOf(["Admin","User"],"Role must be Admin or User").notRequired()
+});
+
+const loginSchema = yup.object({
+    email:yup.string().email("Invalid email").required("Email is required"),
+    password:yup.string().required("Password is required"),
+});
 
 export const signup = async (req:Request,res:Response):Promise<void> => {
     const { email,password,role } = req.body;
     try{
+
+        await signupSchema.validate(req.body,{abortEarly:false});
+
         const existingUser = await User.findOne({ email });
         if(existingUser){
             res.status(400).json({message:'User already exists'});
@@ -16,14 +31,21 @@ export const signup = async (req:Request,res:Response):Promise<void> => {
         await user.save();
 
         res.status(201).json({message:'User Created'});
-    }catch(error){
+    }catch(error:any){
+        if(error.name==="ValidationError"){
+            res.status(400).json({message:'Validation Failed',errors:error.errors});
+        }else{
         res.status(500).json({message:"Server error"});
+        }
     };
 };
 
 export const login = async (req:Request,res:Response) => {
     const {email,password} = req.body;
     try{
+
+        await loginSchema.validate(req.body, { abortEarly: false });
+
         const user = await User.findOne({email});
         if(!user){
             res.status(400).json({message:'Invalid Credentials'});
@@ -37,7 +59,11 @@ export const login = async (req:Request,res:Response) => {
 
         const token = jwt.sign({userId:user._id},process.env.JWT_SECRET as string,{expiresIn:'1d'});
         res.json(token);
-    }catch(error){
+    }catch(error:any){
+        if(error.name === 'ValidationError'){
+            res.status(400).json({message:'Validation Failed',errors:error.errors});
+        }else{
         res.status(500).json({message:"Server error"});
+        }
     }
 };
